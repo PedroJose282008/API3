1f2850994de50e645afa6604ae889a969e386562
Pedro.José <pedrojose2008gtr@gmail.com>
2025-10-08 22:29:42 -0300

    ADD o comando put, que é quem atualiza

--- src/main/java/lab/crud/api/controller/ProdutoController.java
+++ src/main/java/lab/crud/api/controller/ProdutoController.java
@@ -1,29 +1,30 @@
 package lab.crud.api.controller;
-
+ 
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lab.crud.api.model.Produto;
 import lab.crud.api.repository.ProdutoRepository;
-
+ 
 @RestController
 public class ProdutoController {
-	
-	
-
-	
+ 
 	@Autowired
 	private ProdutoRepository repository;
-	//curl -X POST http://localhost:8080/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
+	//curl -X POST http://localhost:8081/produtos -H "Content-Type: application/json; Charset=utf-8" -d @produto-pao.json
 	
 	//@RequestMapping(method = RequestMethod.POST, path = "/produto")
+	
 	@PostMapping("/produtos")
 	public ResponseEntity<Produto> novo(
 			@RequestBody Produto produto) {
@@ -34,7 +35,6 @@ public class ProdutoController {
 		
 		System.out.println(produto.toString());
 		
-		
 		return ResponseEntity
 				.status(HttpStatus.CREATED)
 				.body(produto);
@@ -42,9 +42,55 @@ public class ProdutoController {
 	
 	@GetMapping("/produtos")
 	public ResponseEntity<Iterable<Produto>> obterTodos(){
-		return  ResponseEntity
+		return ResponseEntity
 				.status(HttpStatus.OK)
 				.body(repository.findAll());
-		
 	}
-}
+	
+	@GetMapping("/produtos/{id}")
+	public ResponseEntity<Object> buscarPorId(
+			@PathVariable Integer id) {
+		
+		//Alt + L
+		Optional<Produto>produtoEncontrado = repository.findById(id);
+			
+		//Empty = Vazio
+		if (produtoEncontrado.isEmpty()) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("Produto não encontrado");
+		}
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(produtoEncontrado.get());
+	}
+	
+	
+	@PutMapping("/produtos/{id}")
+	public ResponseEntity<Object> atualizarProduto(
+			@PathVariable Integer id,
+			@RequestBody Produto prod){
+	
+		//Alt + SHIFT + L -> extrai variável local
+		Optional<Produto> produto = repository.findById(id);
+		
+		//Empty = Vazio
+		if(produto.isEmpty()) {
+			
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("Produto não encontrado!");
+		}
+		prod.setId(id);
+		prod.setDataCriacao(produto.get().getDataCriacao());
+		repository.save(prod);
+		
+		
+		
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body("Produto atualizado com sucesso");
+	
+	}
+	
+} 
